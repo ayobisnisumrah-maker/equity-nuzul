de3b2e8fbe81e707267934cb0449c1ddf80e8f60
ayobisnisumrah-maker <ayobisnisumrah@gmail.com>
2026-09-25 21:22:51 +0800

    fix: preserve original portal content and behavior

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { PortalContentProvider } from './context/PortalContentContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PortalContentProvider>
-      <App />
-    </PortalContentProvider>
+    <App />
   </StrictMode>,
 );
