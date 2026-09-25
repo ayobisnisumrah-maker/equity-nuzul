38c0b9a297e3e0ab14cd05370c88716b2fc1f77a
ayobisnisumrah-maker <ayobisnisumrah@gmail.com>
2026-09-26 02:48:35 +0800

    feat: wire portal CMS provider without layout changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { PortalContentProvider } from './context/PortalContentContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PortalContentProvider>
+      <App />
+    </PortalContentProvider>
   </StrictMode>,
 );
